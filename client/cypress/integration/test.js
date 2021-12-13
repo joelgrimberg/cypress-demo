6c746ec71d63d1711a9d5ee48c38c8b9a06c6009
Joël <jgrimberg@qxperts.io>
2021-12-13 10:15:48 +0100

    add server

--- client/cypress/integration/test.js
+++ client/cypress/integration/test.js
@@ -1,6 +1,10 @@
 describe("some test", () => {
-  it("should do something", () => {
+  it("should test client", () => {
     cy.visit("/");
     cy.contains(/joël/i);
+  });
+  it("should test server", () => {
+    cy.visit("http://localhost:8000");
+    cy.contains(/server/i);
   });
 });
